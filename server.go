6e2b1c252c9bd0413b950308eebdd750f79c57fe
lonshan3010 <lonshan3010@gmail.com>
2025-10-10 22:36:34 +0000

    Changes before Firebase Studio auto-run

--- server.go
+++ server.go
@@ -1,30 +1,21 @@
-// Copyright 2023 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Hello is a simple hello, world demonstration web server.
-//
-// It serves version information on /version and answers
-// any other request like /name by saying "Hello, name!".
-//
-// See golang.org/x/example/outyet for a more sophisticated server.
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
-	"html"
 	"log"
-	"net/http"
 	"os"
 	"runtime/debug"
-	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 )
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "usage: helloserver [options]\n")
+	fmt.Println("usage: helloserver [options]")
 	flag.PrintDefaults()
-	os.Exit(2)
 }
 
 var (
@@ -33,43 +24,53 @@ var (
 )
 
 func main() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file, using environment variables directly")
+	}
+
 	// Parse flags.
 	flag.Usage = usage
 	flag.Parse()
 
-	// Parse and validate arguments (none).
-	args := flag.Args()
-	if len(args) != 0 {
-		usage()
+	// Create a new Fiber app.
+	app := fiber.New()
+
+	// Connect to the database.
+	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
+	defer dbpool.Close()
 
 	// Register handlers.
-	// All requests not otherwise mapped with go to greet.
-	// /version is mapped specifically to version.
-	http.HandleFunc("/", greet)
-	http.HandleFunc("/version", version)
+	app.Get("/version", func(c *fiber.Ctx) error {
+		info, ok := debug.ReadBuildInfo()
+		if !ok {
+			return c.Status(500).SendString("no build information available")
+		}
+		c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
+		return c.SendString("<pre>\n" + info.String() + "</pre>\n")
+	})
 
+	app.Get("/db", func(c *fiber.Ctx) error {
+		err := dbpool.Ping(context.Background())
+		if err != nil {
+			return c.Status(500).SendString("Database ping failed: " + err.Error())
+		}
+		return c.SendString("Database ping successful!")
+	})
+
+	app.Get("/:name?", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		if name == "" {
+			name = "Gopher"
+		}
+		return c.SendString(fmt.Sprintf("%s, %s!", *greeting, name))
+	})
+
+	// Start the server.
 	log.Printf("serving http://%s\n", *addr)
-	log.Fatal(http.ListenAndServe(*addr, nil))
-}
-
-func version(w http.ResponseWriter, r *http.Request) {
-	info, ok := debug.ReadBuildInfo()
-	if !ok {
-		http.Error(w, "no build information available", 500)
-		return
-	}
-
-	fmt.Fprintf(w, "<!DOCTYPE html>\n<pre>\n")
-	fmt.Fprintf(w, "%s\n", html.EscapeString(info.String()))
-}
-
-func greet(w http.ResponseWriter, r *http.Request) {
-	name := strings.Trim(r.URL.Path, "/")
-	if name == "" {
-		name = "Gopher"
-	}
-
-	fmt.Fprintf(w, "<!DOCTYPE html>\n")
-	fmt.Fprintf(w, "%s, %s!\n", *greeting, html.EscapeString(name))
+	log.Fatal(app.Listen(*addr))
 }
